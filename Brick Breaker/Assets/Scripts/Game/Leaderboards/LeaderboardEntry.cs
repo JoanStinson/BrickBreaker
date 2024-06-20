210e735b575eff14ba03dd9393d27d2f29d486dd
Joan Ginard Mateo <jgm7.hd@gmail.com>
2024-06-20 11:20:01 +0200

    Infinite scroll working but cells cutoff

---
+++ Brick Breaker/Assets/Scripts/Game/Leaderboards/LeaderboardEntry.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace JGM.Game
+{
+    [Serializable]
+    public class LeaderboardEntry
+    {
+        public string Name;
+        public int Score;
+    }
+}
--- Brick Breaker/Assets/Scripts/Game/Leaderboards/LeaderboardsView.cs
+++ Brick Breaker/Assets/Scripts/Game/Leaderboards/LeaderboardsView.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 
 namespace JGM.Game
@@ -8,10 +9,17 @@ namespace JGM.Game
         [SerializeField] private Button m_playAgainButton;
         [SerializeField] private Button m_mainMenuButton;
         [SerializeField] private Button m_quitButton;
-        [SerializeField] private Transform m_scrollContent;
-        [SerializeField] private LeaderboardCellView m_cell;
+        [SerializeField] private ScrollRect m_scrollRect;
+        [SerializeField] private LeaderboardCellView m_cellPrefab;
 
         private GameView m_gameView;
+        private List<LeaderboardEntry> m_entries;
+        private List<LeaderboardCellView> m_cellsPool;
+        private int m_totalEntries = 100;
+        private int m_visibleCellsCount = 10;
+        private float m_cellHeight;
+
+        private int m_firstVisibleIndex = 0;
 
         public override void Initialize(GameView gameView)
         {
@@ -39,13 +47,65 @@ namespace JGM.Game
         public override void Show()
         {
             base.Show();
+            InitializeLeaderboard();
+            UpdateCells();
+        }
 
-            int userScore = m_gameView.Model.Score * m_gameView.Model.ScoreMultiplier;
-            var cell = GameObject.Instantiate(m_cell);
-            cell.transform.SetParent(m_scrollContent, false);
-            int position = 0;
-            string name = "Pepe";
-            cell.SetText($"Position {position} - {name} - {userScore}");
+        private void InitializeLeaderboard()
+        {
+            m_entries = GenerateRandomEntries(m_totalEntries);
+            m_cellsPool = new List<LeaderboardCellView>();
+            m_cellHeight = ((RectTransform)m_cellPrefab.transform).rect.height;
+
+            for (int i = 0; i < m_visibleCellsCount; i++)
+            {
+                var cell = Instantiate(m_cellPrefab, m_scrollRect.content);
+                m_cellsPool.Add(cell);
+            }
+
+            m_scrollRect.onValueChanged.AddListener(OnScrollChanged);
+            m_scrollRect.content.sizeDelta = new Vector2(m_scrollRect.content.sizeDelta.x, m_cellHeight * m_totalEntries);
+        }
+
+        private List<LeaderboardEntry> GenerateRandomEntries(int count)
+        {
+            List<LeaderboardEntry> entries = new List<LeaderboardEntry>();
+            System.Random rand = new System.Random();
+            for (int i = 0; i < count; i++)
+            {
+                string name = "User" + rand.Next(1000, 9999);
+                int score = rand.Next(1000, 100000);
+                entries.Add(new LeaderboardEntry { Name = name, Score = score });
+            }
+
+            entries.Sort((a, b) => b.Score.CompareTo(a.Score));
+            return entries;
+        }
+
+        private void OnScrollChanged(Vector2 position)
+        {
+            int newFirstVisibleIndex = Mathf.FloorToInt(m_scrollRect.content.anchoredPosition.y / m_cellHeight);
+            if (newFirstVisibleIndex != m_firstVisibleIndex)
+            {
+                UpdateCells();
+            }
+        }
+
+        private void UpdateCells()
+        {
+            int newFirstVisibleIndex = Mathf.FloorToInt(m_scrollRect.content.anchoredPosition.y / m_cellHeight);
+
+            for (int i = 0; i < m_visibleCellsCount; i++)
+            {
+                int entryIndex = newFirstVisibleIndex + i;
+                if (entryIndex >= 0 && entryIndex < m_totalEntries)
+                {
+                    m_cellsPool[i].SetText($"Position {entryIndex + 1} - {m_entries[entryIndex].Name} - {m_entries[entryIndex].Score}");
+                    m_cellsPool[i].transform.localPosition = new Vector3(0, -entryIndex * m_cellHeight, 0);
+                }
+            }
+
+            m_firstVisibleIndex = newFirstVisibleIndex;
         }
     }
 }
